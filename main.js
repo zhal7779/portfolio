cfaeedfe8582c8213d2dda0b7c23623016f3801a
zhal7779 <79049046+zhal7779@users.noreply.github.com>
2022-03-05 02:53:15 +0900

    JS: Home transparent

--- main.js
+++ main.js
@@ -30,6 +30,19 @@ contactMe.addEventListener('click', (event) => {
     scrollIntoView('#contact');
 });
 
+//Make home slowly fade totransparent as the window scrolls down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+
+
+
+
+
+
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: 'smooth'});
