2d1564b023be35f61c83885c7563e3218711d63f
Minyoung Lee <zhal7779@gmail.com>
2023-09-11 00:31:16 +0900

    Feat: JS - 스크롤시 섹션 id와 동일한 메뉴 활성화 기능 구현

---
+++ src/activeMenu.js
@@ -0,0 +1,50 @@
+'use strict!';
+// 스크롤시 섹션id와 동일한 메뉴 활성화
+const sectionIds = [
+  '#home',
+  '#about',
+  '#skills',
+  '#projects',
+  '#otherExperience',
+  '#contact',
+];
+const MenuItems = sectionIds.map((id) =>
+  document.querySelector(`[href="${id}"]`)
+);
+
+const menuIndexes = new Array(sectionIds.length).fill(false);
+
+const observerOptions = {
+  threshold: [0, 0.98],
+  rootMargin: '-30% 0px 0px 0px',
+};
+const observerCallback = (entries) => {
+  let lastMenu;
+
+  entries.forEach((entry) => {
+    const currentId = entry.target.id;
+    const currentIndexes = sectionIds.indexOf(`#${currentId}`);
+    menuIndexes[currentIndexes] = entry.isIntersecting;
+    lastMenu =
+      currentIndexes === sectionIds.length - 1 && entry.intersectionRatio > 0.9;
+  });
+
+  const currentIndex = lastMenu
+    ? sectionIds.length - 1
+    : menuIndexes.indexOf(true);
+
+  selectedMenu(MenuItems, currentIndex);
+};
+
+function selectedMenu(MenuItems, currentIndex) {
+  MenuItems.forEach((menuItem, index) => {
+    if (index !== currentIndex) {
+      menuItem.classList.remove('active');
+    }
+  });
+  MenuItems[currentIndex].classList.add('active');
+}
+
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+sectionIds.forEach((id) => observer.observe(document.querySelector(id)));
